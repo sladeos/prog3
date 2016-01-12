a505ea70ae4bbc6aec66e027cf1b158674a76af2
Sebastian Nisser Blanck <sebbenb@gmail.com>
2016-01-12 14:38:47 +0100

    Cheeky bugger

--- prog3engine/prog3engine/Level.cpp
+++ prog3engine/prog3engine/Level.cpp
@@ -15,22 +15,25 @@ namespace gengine {
 		sprites.push_back(background);
 	}
 
+	//Add a sprite to the level
 	void gengine::Level::addSprite(Sprite *sprite)
 	{
 		sprites.push_back(sprite);
 	}
 
-
+	//Add sprites (This function takes a vector) 
 	void gengine::Level::addSprites(std::vector<Sprite*> spritesToLoad)
 	{
 		sprites.insert(sprites.end(), spritesToLoad.begin(), spritesToLoad.end());
 	}
 
+	//Removes a sprite from the level
 	void gengine::Level::removeSprite(Sprite *s)
 	{
 		sprites.erase(std::remove(sprites.begin(), sprites.end(), s), sprites.end());
 	}
 
+	//Set the background for the level
 	void Level::setBackground(FixedSprite *newBackground)
 	{
 		removeSprite(background);
@@ -39,12 +42,14 @@ namespace gengine {
 
 	}
 
+	//Return the list of sprites that can be loaded with this level
 	std::vector<Sprite*> Level::getSprites() const
 	{
 		
 		return sprites;
 	}
 
+	//Delete the sprites in this level
 	void Level::deleteObjects()
 	{
 		for (Sprite* s : sprites) {
@@ -52,7 +57,7 @@ namespace gengine {
 		}
 		sprites.clear();
 	}
-
+	//Destructor
 	Level::~Level()
 	{
 		deleteObjects();
